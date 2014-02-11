0b5a8fa22f78b6c8161a72954d23e659247ecfa4
Logan Hasson <logan.hasson@gmail.com>
2014-02-11 15:52:11 -0500

    Fix spec

--- spec/cash_register_spec.rb
+++ spec/cash_register_spec.rb
@@ -14,16 +14,6 @@ describe 'CashRegister' do
     end
   end
 
-  describe '#add_item' do
-    it 'accepts a title and a price and increases the total' do
-      expect{cash_register.add_item("eggs", 0.98)}.to increase(cash_register.total).by(0.98)
-    end
-
-    it 'also accepts an optional quantity' do
-      expect{cash_register.add_item("book", 5.00, 3)}.to increase(cash_register.total).by(15.00)
-    end
-  end
-
   describe '#total' do
     it 'returns the current total' do
       cash_register.total = 0
@@ -31,6 +21,16 @@ describe 'CashRegister' do
       cash_register.add_item("gloves", 15.99)
 
       expect(cash_register.total).to eq(21.55)
+    end
+  end
+
+  describe '#add_item' do
+    it 'accepts a title and a price and increases the total' do
+      expect{cash_register.add_item("eggs", 0.98)}.to change{cash_register.total}.by(0.98)
+    end
+
+    it 'also accepts an optional quantity' do
+      expect{cash_register.add_item("book", 5.00, 3)}.to change{cash_register.total}.by(15.00)
     end
   end
 
@@ -44,13 +44,14 @@ describe 'CashRegister' do
       it 'reduces the total' do
         cash_register.total = 0
         cash_register_with_discount.add_item("macbook air", 1000)
-        expect{cash_register_with_discount.apply_discount}.to reduce(cash_register_with_discount.total).by(200)
+        expect{cash_register_with_discount.apply_discount}.to change{cash_register_with_discount.total}.by(-200)
       end
     end
 
     context 'the cash register was not initialized with an employee discount' do
       it 'says that there is no discount to apply' do
         expect($stdout).to receive(:puts).with("There is no discount to apply.")
+        cash_register.apply_discount
       end
     end
   end
@@ -67,7 +68,7 @@ describe 'CashRegister' do
   describe '#void_last_transaction' do
     it 'subtracts the last transaction from the total' do
       cash_register.add_item("tomato", 1.76)
-      expect{cash_register.void_last_transaction}.to reduce(cash_register.total).by(1.76)
+      expect{cash_register.void_last_transaction}.to change{cash_register.total}.by(-1.76)
     end
   end
 end

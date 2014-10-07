f25d4e76328057067f1656f368625ca8745d27ed
Katie Hoffman <ktahoffman@gmail.com>
2014-10-07 14:50:13 -0400

    pass new test for string

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,27 +1,12 @@
-require 'pry'
 class CashRegister
 
+  attr_accessor :items, :last_transaction_amount, :discount, :total
+
   def initialize(discount=0)
-    self.total = 0
-    self.discount = discount
-    self.items = []
-    self.last_transaction_amount = 0
-  end
-
-  def total=(value)
-    @total = value
-  end
-
-  def discount=(value)
-    @discount = value
-  end
-
-  def discount
-    @discount
-  end
-
-  def total
-    @total
+    @total = 0
+    @discount = discount
+    @items = []
+    @last_transaction_amount = 0
   end
 
   def add_item(title, amount, quantity=1)
@@ -29,32 +14,16 @@ class CashRegister
     quantity.times do
       items << title
     end
-
     self.last_transaction_amount = amount * quantity
   end
 
   def apply_discount
     if discount != 0
       self.total = (total * ((100.0 - discount.to_f)/100)).to_i
+      "After the discount, the total comes to $#{self.total}."
     else
       puts "There is no discount to apply."
     end
-  end
-
-  def items=(list)
-    @items = list
-  end
-
-  def items
-    @items
-  end
-
-  def last_transaction_amount=(amount)
-    @last_transaction_amount = amount
-  end
-
-  def last_transaction_amount
-    @last_transaction_amount
   end
 
   def void_last_transaction

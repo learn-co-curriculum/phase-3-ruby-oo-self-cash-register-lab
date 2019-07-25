958a836940904582ca21b35d50410bf2d37e5bbb
maxwellbenton <maxwellbenton@gmail.com>
2019-07-25 11:42:34 -0400

    updates solution, remove extra solution file

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,11 +1,26 @@
 class CashRegister
 
-  attr_accessor :items, :discount, :total, :last_transaction
-
   def initialize(discount=0)
-    @total = 0
-    @discount = discount
-    @items = []
+    self.total = 0
+    self.discount = discount
+    self.items = []
+    self.last_transaction_amount = 0
+  end
+
+  def total=(value)
+    @total = value
+  end
+
+  def discount=(value)
+    @discount = value
+  end
+
+  def discount
+    @discount
+  end
+
+  def total
+    @total
   end
 
   def add_item(title, amount, quantity=1)
@@ -13,7 +28,8 @@ class CashRegister
     quantity.times do
       items << title
     end
-    self.last_transaction = amount * quantity
+
+    self.last_transaction_amount = amount * quantity
   end
 
   def apply_discount
@@ -25,7 +41,23 @@ class CashRegister
     end
   end
 
+  def items=(list)
+    @items = list
+  end
+
+  def items
+    @items
+  end
+
+  def last_transaction_amount=(amount)
+    @last_transaction_amount = amount
+  end
+
+  def last_transaction_amount
+    @last_transaction_amount
+  end
+
   def void_last_transaction
-    self.total = self.total - self.last_transaction
+    self.total -= last_transaction_amount
   end
 end

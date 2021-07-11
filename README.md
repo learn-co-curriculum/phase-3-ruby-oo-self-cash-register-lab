09191442d35147d2d48cf34b43c2b309a94c58bf
ihollander <ianholla@gmail.com>
2021-07-11 11:58:44 -0400

    Fix formatting

--- README.md
+++ README.md
@@ -10,18 +10,23 @@
 
 We're going to create an Object Oriented Cash Register that can:
 
-* Add items of varying quantities and prices
-* Calculate discounts
-* Keep track of what's been added to it
-* Void the last transaction
+- Add items of varying quantities and prices
+- Calculate discounts
+- Keep track of what's been added to it
+- Void the last transaction
 
 ## Instructions
 
-**This is a test-driven lab!** You will need to read the spec file and the test output very carefully to solve this one.
+**This is a test-driven lab!** You will need to read the spec file and the test
+output very carefully to solve this one.
 
-Note that a discount is calculated as a percentage off of the total cash register price (e.g. a discount of 20 means the customer receives 20% off of their total price).
+Note that a **discount** is calculated as a percentage off of the total cash
+register price (e.g. a discount of 20 means the customer receives 20% off of
+their total price).
 
-**Hint #1:** Keep in mind that to call an instance method *inside* another instance method, we use the `self` keyword to refer to the instance on which we are operating. For example:
+**Hint #1:** Keep in mind that to call an instance method _inside_ another
+instance method, we use the `self` keyword to refer to the instance on which we
+are operating. For example:
 
 ```ruby
 class Person
@@ -38,10 +43,17 @@ class Person
 end
 ```
 
-Follow along with the specs in `spec/cash_register_spec.rb`. Reading along with what the tests are looking for can be really helpful!
+Follow along with the specs in `spec/cash_register_spec.rb`. Reading along with
+what the tests are looking for can be really helpful!
 
 Take it one step at a time!
 
-**Hint #2:** The `apply_discount` requires some knowledge about working with an Integer versus a Float in Ruby. Note that `100.class` returns Integer while `100.0.class` returns Float.  Ruby provides methods for changing an object of type Integer to a Float and vice versa.
+**Hint #2:** The `apply_discount` requires some knowledge about working with an
+Integer versus a Float in Ruby. Note that `100.class` returns Integer while
+`100.0.class` returns Float. Ruby provides methods for changing an object of
+type Integer to a Float and vice versa.
 
-**Hint #3:** The `void_last_transaction` method will remove the last transaction from the total. You'll need to make an additional attribute accessor and keep track of that last transaction amount somehow. In what method of the class are you working with an individual item?
+**Hint #3:** The `void_last_transaction` method will remove the last transaction
+from the total. You'll need to make an additional attribute accessor and keep
+track of that last transaction amount somehow. In what method of the class are
+you working with an individual item?
--- spec/cash_register_spec.rb
+++ spec/cash_register_spec.rb
@@ -2,12 +2,12 @@ describe 'CashRegister' do
   let(:cash_register) { CashRegister.new }
   let(:cash_register_with_discount) { CashRegister.new(20) }
 
-  describe '::new' do
+  describe '.new' do
     it 'sets an instance variable @total on initialization to zero' do
       expect(cash_register.instance_variable_get(:@total)).to eq(0)
     end
 
-    it 'optionally takes an employee discount on initialization' do
+    it 'takes one optional argument, an employee discount, on initialization' do
       expect(cash_register_with_discount.discount).to eq(20)
     end
   end
@@ -21,11 +21,11 @@ describe 'CashRegister' do
 
   describe '#add_item' do
     it 'accepts a title and a price and increases the total' do
-      expect{cash_register.add_item("eggs", 0.98)}.to change{cash_register.total}.by(0.98)
+      expect { cash_register.add_item("eggs", 0.98) }.to change(cash_register, :total).by(0.98)
     end
 
     it 'also accepts an optional quantity' do
-      expect{cash_register.add_item("book", 5.00, 3)}.to change{cash_register.total}.by(15.00)
+      expect { cash_register.add_item("book", 5.00, 3) }.to change(cash_register, :total).by(15.00)
     end
 
     it "doesn't forget about the previous total" do
@@ -39,7 +39,7 @@ describe 'CashRegister' do
   end
 
   describe '#apply_discount' do
-    context 'the cash register was initialized with an employee discount' do
+    context 'when the cash register was initialized with an employee discount' do
       it 'applies the discount to the total price' do
         cash_register_with_discount.add_item("macbook air", 1000)
         cash_register_with_discount.apply_discount
@@ -54,11 +54,11 @@ describe 'CashRegister' do
       it 'reduces the total' do
         cash_register.total = 0
         cash_register_with_discount.add_item("macbook air", 1000)
-        expect{cash_register_with_discount.apply_discount}.to change{cash_register_with_discount.total}.by(-200)
+        expect { cash_register_with_discount.apply_discount }.to change(cash_register_with_discount, :total).by(-200)
       end
     end
 
-    context 'the cash register was not initialized with an employee discount' do
+    context 'when the cash register was not initialized with an employee discount' do
       it 'returns a string error message that there is no discount to apply' do
         expect(cash_register.apply_discount).to eq("There is no discount to apply.")
       end
@@ -66,11 +66,22 @@ describe 'CashRegister' do
   end
 
   describe '#items' do
-    it 'returns an array containing all items that have been added' do
-      new_register = CashRegister.new
-      new_register.add_item("eggs", 1.99)
-      new_register.add_item("tomato", 1.76, 3)
-      expect(new_register.items).to eq(["eggs", "tomato", "tomato", "tomato"])
+    context 'when called with two arguments' do
+      it 'returns an array containing all items that have been added' do
+        new_register = CashRegister.new
+        new_register.add_item("eggs", 1.99)
+        new_register.add_item("tomato", 1.76)
+        expect(new_register.items).to eq(["eggs", "tomato"])
+      end
+    end
+
+    context 'when called with three arguments' do
+      it 'returns an array containing all items that have been added, including multiples' do
+        new_register = CashRegister.new
+        new_register.add_item("eggs", 1.99, 2)
+        new_register.add_item("tomato", 1.76, 3)
+        expect(new_register.items).to eq(["eggs", "eggs", "tomato", "tomato", "tomato"])
+      end
     end
   end
 
@@ -84,7 +95,7 @@ describe 'CashRegister' do
 
     it 'returns the total to 0.0 if all items have been removed' do
       cash_register.add_item("tomato", 1.76, 2)
-      expect{cash_register.void_last_transaction}.to change{cash_register.total}.from(3.52).to(0.0)
+      expect { cash_register.void_last_transaction }.to change(cash_register, :total).from(3.52).to(0.0)
     end
   end
 end

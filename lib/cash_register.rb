149a98130cff77d9392ecedca07a06af14e5bb43
ihollander <ianholla@gmail.com>
2021-07-11 12:04:06 -0400

    Format solution

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,23 +2,24 @@ class CashRegister
 
   attr_accessor :items, :discount, :total, :last_transaction
 
-  def initialize(discount=0)
+  def initialize(discount = 0)
     @total = 0
     @discount = discount
     @items = []
   end
 
-  def add_item(title, amount, quantity=1)
-    self.total += amount * quantity
-    quantity.times do
-      items << title
-    end
+  def add_item(title, amount, quantity = 1)
     self.last_transaction = amount * quantity
+    self.total += self.last_transaction
+    quantity.times do
+      self.items << title
+    end
   end
 
   def apply_discount
-    if discount != 0
-      self.total = (total * ((100.0 - discount.to_f)/100)).to_i
+    if self.discount != 0
+      discount_as_percent = (100.0 - self.discount.to_f) / 100
+      self.total = (self.total * discount_as_percent).to_i
       "After the discount, the total comes to $#{self.total}."
     else
       "There is no discount to apply."
@@ -26,6 +27,6 @@ class CashRegister
   end
 
   def void_last_transaction
-    self.total = self.total - self.last_transaction
+    self.total -= self.last_transaction
   end
 end

6da6b6df33dd9e576571564102c300fdd7d8e4e1
Silvia-Mutete <silvia.mutete@student.moringaschool.com>
2022-08-18 13:07:42 +0300

    commit tree 00d7e6008a5c876bdbbbabba3a5a739845e65908

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,17 @@
+class CashRegister
+     attr_accessor :total, :discount, :price
 
+    def initialize (discount= 0)
+        @total = 0
+        @discount = discount
+    end
+    def add_item(title,price, quantity=1)
+        self.total += (price * quantity)
+        count = 0
+        while count < quantity
+            self.items.push(title)
+            count += 1
+        end
+        self.price = price * quantity
+    end
+end

15ae7c8591d3f14b3cba52e43a19927f258ebaf7
otieno-derrick <otieno.derrrick@student.moringaschool.com>
2022-08-24 09:55:23 +0300

    done

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -32,8 +32,26 @@ class CashRegister
 
    def void_last_transaction
      self.total -=  self.last_transaction
-    
-   end
+
+   endclass Car
+
+  attr_accessor :name, :owner
+
+  def initialize(name)
+    @name = name
+  end
+
+  def honk
+    "Beep!"
+  end
+
+  def take_ownership(owner_name)
+    self.owner = owner_name
+  end
+
+end
+
+lancelot = Car.new("2003 Mitsubishi Lancer")~
 
 
 
